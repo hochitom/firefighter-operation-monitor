2265978ae50ca3756ea5fad637ac104ae38c9f19
hochitom <hochitom@icloud.com>
2015-01-19 17:38:49 +0100

    created simple http server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var ecstatic = require('ecstatic'),
-    http = require('http').createServer(
-        ecstatic({ root: __dirname + '/public' })
-    ),
-    io = require('socket.io').listen(http),
-    mail = require('./get-mails').startChecking(io);
+'use strict';
 
-http.listen(3000);
+var config = require('./config');
+var http = require('http');
+var checkMails = require('./get-mails')();
 
-console.log('Listening on :3000');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(config.port, '127.0.0.1');
+
+console.log('Listening on :' + config.port);
