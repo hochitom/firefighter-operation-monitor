66c5a0886e315317e908f2fb5053604838ba2bd2
hochitom <hochitom@icloud.com>
2015-01-19 18:17:23 +0100

    added config sample file

---
+++ config-sample.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    'port': 3000, // port where the app runs
+    'db': 'mongodb://localhost/ff-monit', // mongodb url
+    'user': '', // mail user
+    'password': '', // mail password
+    'host': '' // host of mail server
+};
