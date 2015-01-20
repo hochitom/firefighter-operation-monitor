3043d35156fb11c83420353b80fcc4b40116f3a6
hochitom <hochitom@icloud.com>
2015-01-20 10:41:21 +0100

    added config file

---
+++ config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    'port': 3000,
+    'db': process.env.db,
+    'user': process.env.user,
+    'password': process.env.password,
+    'host': process.env.host
+};
