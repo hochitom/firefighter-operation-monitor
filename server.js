b605c2deaf601011b8bd79dca36a3b1bb091aca2
hochitom <hochitom@icloud.com>
2015-04-05 19:20:42 +0200

    fixed bug

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ http.createServer(function (req, res) {
 
             res.writeHead(200, {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin', 'http://localhost:8000'
+                'Access-Control-Allow-Origin': 'http://localhost:8000'
             });
 
             res.end(JSON.stringify(emergency));
